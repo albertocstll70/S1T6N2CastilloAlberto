7aa4c4843e94fb3b0f04945eb15d99dfe8bcf042
alberto <albertocstll70@gmail.com>
2022-09-21 18:52:02 +0200

    S1T6N2CastilloAlberto/

--- S1T6N2CastilloAlberto/src/s1t6n2exercici2/GenericMethods.java
+++ S1T6N2CastilloAlberto/src/s1t6n2exercici2/GenericMethods.java
@@ -1,35 +1,16 @@
 package s1t6n2exercici2;
 
-
 public class GenericMethods<T> {
-//
-//	public static void generic(List<?> lista) {
-//
-//		for (Object e : lista) {
-//			System.out.println(e);
-//
-//		}
-//
-//	}
-	
-	@SuppressWarnings("unchecked")
-	public static <T> void generic( T...argumento) {
-		
-		for(T t: argumento) {
-			
-			System.out.println(t);
-			
-			
-		}
-		
-		
-		
-		
-		
-	}
 
-	
-	
-	
+	@SuppressWarnings("unchecked")
+	public static <T> void generic(T... argumento) {
+
+		for (T t : argumento) {
+
+			System.out.println(t);
+
+		}
+
+	}
 
 }
--- S1T6N2CastilloAlberto/src/s1t6n2exercici2/MainExercici2.java
+++ S1T6N2CastilloAlberto/src/s1t6n2exercici2/MainExercici2.java
@@ -20,21 +20,17 @@ public class MainExercici2 {
 		GenericMethods<Object> generico1 = new GenericMethods<Object>();
 
 		ArrayList<String> lista = new ArrayList<String>();
-		
+
 		lista.add("Juan");
 		lista.add("Pedro");
 		lista.add("Montserrat");
 		lista.add("George");
 
-	
-		
-		generico1.generic("Hola mundo","XY",3);
-		
-		generico1.generic("Hola mundo","m",3,1264,25665);
-		
-		generico1.generic(persona1,persona2);
-		
-		
+		generico1.generic("Hola mundo", "XY", 3);
+
+		generico1.generic("Hola mundo", "m", 3, 1264, 25665);
+
+		generico1.generic(persona1, persona2);
 
 	}
 
